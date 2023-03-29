366c47ca14f29aadea230e5fae1db7f494e76190
tgosnnsenpai <107007219+tgosnnsenpai@users.noreply.github.com>
2023-03-29 18:34:41 -0300

    Add files via upload

---
+++ Curriculim_Vitae.js
@@ -0,0 +1,64 @@
+
+
+document.getElementById ('lista1').style.display ='none'
+document.getElementById('Mostrar_menos1').style.display='none'
+
+
+
+document.getElementById('Mostrar_mas1').addEventListener('click', function(){
+
+    document.getElementById ('lista1').style.display =''
+    document.getElementById('Mostrar_mas1').style.display='none'
+    document.getElementById('Mostrar_menos1').style.display=''
+
+})
+
+document.getElementById('Mostrar_menos1').addEventListener('click', function(){
+
+    document.getElementById ('lista1').style.display ='none'
+    document.getElementById('Mostrar_menos1').style.display='none'
+    document.getElementById('Mostrar_mas1').style.display=''
+
+})
+
+document.getElementById ('lista2').style.display ='none'
+document.getElementById('Mostrar_menos2').style.display='none'
+
+
+
+document.getElementById('Mostrar_mas2').addEventListener('click', function(){
+
+    document.getElementById ('lista2').style.display =''
+    document.getElementById('Mostrar_mas2').style.display='none'
+    document.getElementById('Mostrar_menos2').style.display=''
+
+})
+
+document.getElementById('Mostrar_menos2').addEventListener('click', function(){
+
+    document.getElementById ('lista2').style.display ='none'
+    document.getElementById('Mostrar_menos2').style.display='none'
+    document.getElementById('Mostrar_mas2').style.display=''
+
+})
+
+document.getElementById ('Res_prof').style.display ='none'
+document.getElementById('Mostrar_menos3').style.display='none'
+
+
+
+document.getElementById('Mostrar_mas3').addEventListener('click', function(){
+
+    document.getElementById ('Res_prof').style.display =''
+    document.getElementById('Mostrar_mas3').style.display='none'
+    document.getElementById('Mostrar_menos3').style.display=''
+
+})
+
+document.getElementById('Mostrar_menos3').addEventListener('click', function(){
+
+    document.getElementById ('Res_prof').style.display ='none'
+    document.getElementById('Mostrar_menos3').style.display='none'
+    document.getElementById('Mostrar_mas3').style.display=''
+
+})
